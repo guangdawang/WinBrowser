c31cd6f81fdacb3366da307d123ded728f5469e7
guangadawang <2658303227@qq.com>
2026-01-06 14:04:50 +0800

    添加缺失的源文件bookmark.cpp和settings.cpp

---
+++ cpp/src/models/bookmark.cpp
@@ -0,0 +1,7 @@
+
+#include "bookmark.h"
+
+namespace WinBrowserQt {
+// Bookmark类的实现目前仅包含头文件中的内联函数
+// 这里保留文件以便未来扩展功能
+} // namespace WinBrowserQt
---
+++ cpp/src/models/settings.cpp
@@ -0,0 +1,7 @@
+
+#include "settings.h"
+
+namespace WinBrowserQt {
+// Settings类的实现目前仅包含头文件中的内联函数
+// 这里保留文件以便未来扩展功能
+} // namespace WinBrowserQt
